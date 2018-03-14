5c1206cd5a601a8f59e65c12bdf09313943c492c
Alan <cingzeon@sina.com>
2018-03-14 10:29:45 +0800

    updata file

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+    <div>
+        <h1>555</h1>
+        <i className="fa fa-camera-retro fa-5x"></i>
+        <h1>Hello, world!</h1>,
+    </div>,
+    document.getElementById('app')
+);
